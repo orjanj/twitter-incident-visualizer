935c80a8fe0d28ef232cee5396d32797e8721801
Ørjan Jacobsen <orjan@orrie.org>
2020-04-03 00:42:26 +0200

    Added start of interface file to web app

---
+++ interface.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+from modules.Config import Config # TODO: Make it prettier
+from modules.DB import DB # TODO: Make it prettier
+from modules.Twitter import TIVTwitter # TODO: Make it prettier
+from modules.File import File
+from modules.Gmaps import Gmaps
+
+# Start of application
+if __name__ == '__main__':
+    config = Config()
+    db = DB(config)
+    gmaps = Gmaps(config)
+    twitter = TIVTwitter(config, db, gmaps)
+
+    # TODO: Dump to JSON FILE
