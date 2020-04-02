cf94af5feb25b09dab26a8208f6eb6e39494871c
Ørjan Jacobsen <orjan@orrie.org>
2020-04-02 22:16:51 +0200

    Cleaned up DB

--- modules/DB.py
+++ modules/DB.py
@@ -1,43 +1,22 @@
 #!/usr/bin/python3
 import psycopg2, sys
-from Config import Config # TODO: Make it prettier
+#from Config import Config # TODO: Make it prettier
 
 class DB:
   """ DB initializes and manipulates PostgreSQL databases."""
 
-  def __init__(self):
+  def __init__(self, config):
     """ Instance constructor for connecting to an existing database.
     :return: none
     """
     self.connected = False
 
     # Initialize configuration parameters for the database
-    config = Config()
-    self.config_param = config.getConfigParameter('postgresql') # TODO: Lag et config objekt kun i mining fila og fjern denne linja fra alle objektfiler
+#    config = Config()
+    self.config_param = config.getConfigParameter('postgresql')
 
     # Connect to database
     self.connect()
-
-  def printError(self, error_message): # TODO: Remove this function if it's not going to be used
-    # Get details about the exception
-    error_type, error_object, traceback = sys.exc_info()
-
-    # Get line number when exception occured
-    line_number = traceback.tb_lineno
-
-    # Print connect() error
-    print("\npsycopg2 ERROR:", error_message, "on line number:", line_number)
-    print("psycopg2 traceback:", traceback,"-- type:", error_type)
-
-    #psycopg2 extensions.Diagonstics object attribute
-    print("\nextensions.Diagnostics:", error_message.diag) # TODO: Check this shiet
-
-    print("pgerror:", error_message.pgerror)      # TODO: Consider if there should be a 'None' check
-    print("pgcode:", error_message.pgcode, "\n")  # TODO: Consider if there should be a 'None' check
-
-    # Exit the program due to an error
-    sys.exit(1)
-
 
   def connect(self):
     """ Connect to the PostgreSQL database. """
@@ -57,7 +36,7 @@ class DB:
     self.connection.close()
     self.connected = False
 
-  def validateConnection(self): # TODO: Consider removing this, probably not going to be used
+  def validateConnection(self): # TODO: REMOVE!!!
     """
     Connection validation to PostgreSQL DB.
     :return: version + DSN parameters (string)
@@ -77,43 +56,28 @@ class DB:
 
     self.close()
 
-
-  def execute(self, statement, values):
-#  def execute(self, statement):
+  def execute(self, statement, values=None):
     """ Execute database query. """
     if self.connected:
-      self.cursor.execute(statement, (values))
-#      self.cursor.execute(statement)
+      try:
+        self.cursor.execute(statement, (values))
+      except psycopg2.Error as error_msg:
+        print(error_msg)
+
       if statement.upper().startswith('SELECT'):
         data = self.cursor.fetchall()
         return data
-#        print(data) # debugging
       else:
         self.connection.commit()
-#        print(self.cursor.rowcount) # debugging
-        return self.cursor.rowcount
+        return(self.cursor.rowcount)
+
+
 
 
 # PoC
-if __name__ == '__main__':
-  db = DB()
-  query = "INSERT INTO account(account_name, account_url, account_text, account_reg_date, account_webpage, account_pic_url, account_verified) VALUES(%s, %s, %s, %s, %s, %s, %s)"
-  values = ('testtest', 'https://lol.com', 'this is a test msg', int(0), 'https://lol.cn', 'http://lol.no', 'false')
-  if db.execute(query, values):
-    print("Row inserted in DB")
-
-# Can also run the following query, to validate the insertion, from the `tiv` user on the server in psql: SELECT * FROM account;
-
-  # Validate connection
-  #db.validateConnection()
-
-
-
-
-# References:
-# https://www.psycopg.org/docs/module.html
-# https://kb.objectrocket.com/postgresql/python-error-handling-with-the-psycopg2-postgresql-adapter-645
-# https://stackoverflow.com/questions/24661754/necessity-of-explicit-cursor-close
-# https://www.psycopg.org/docs/cursor.html
-# https://www.postgresqltutorial.com/postgresql-python/connect/
-# https://codereview.stackexchange.com/questions/134535/a-class-to-create-and-modify-sqlite3-databases-with-a-terminal
+#if __name__ == '__main__':
+#  db = DB()
+#  query = "INSERT INTO account(account_name, account_url, account_text, account_reg_date, account_webpage, account_pic_url, account_verified) VALUES(%s, %s, %s, %s, %s, %s, %s)"
+#  values = ('testtest', 'https://lol.com', 'this is a test msg', int(0), 'https://lol.cn', 'http://lol.no', 'false')
+#  if db.execute(query, values):
+#    print("Row inserted in DB")
