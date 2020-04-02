ae9b8b78af633e056107e76cc2c562a3d992ca37
Ørjan Jacobsen <orjan@orrie.org>
2020-04-02 05:10:53 +0200

    Added file handling methods

--- modules/File.py
+++ modules/File.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+import time
 from Config import Config # TODO: Make it prettier
 
 class File:
@@ -6,16 +7,34 @@ class File:
         """ Main constructor """
         config = Config()
         self.config_param = config.getConfigParameter('tweet_storage')
-        timestamp = ''
+        ts = time.gmtime()
+        timestamp = time.strftime("%Y%m%d%H%M%S", ts)
+#        print(timestamp) # TODO: Consider if timestamp should be set when exporting
         self.export_file = self.config_param['directory'] + '/' + self.config_param['default_filename'] + timestamp + '.' + self.config_param['default_file_extension']
 
 #    def exportToFile(self, tweet_content, filename=self.export_file):
-    def exportToFile(self, tweet_content, filename='tweet-202004010100.json'):
+    def exportToFile(self, tweet_content, filename=None):
         """ Export raw data from Tweets to a file.
         :return: xxx
         """
+        # Open the file in write
+#        with open(filename, "w") as updated_file:
+#            for line in tweet_content:
+#                updated_file.write("{}\n".format(line))
 
-    def importFromFile(self, filename='tweet-202004010100.json'):
+    def importFromFile(self, filename=None):
         """ Import raw data from a file.
+        :param filename: file to use (string)
         :return: raw Tweet data (json)
         """
+        # Open the file in read
+#        with open(filename, "r") as record_file:
+
+            # Go through all lines in the file
+#            for line in record_file.readlines():
+
+
+
+# PoC:
+f = File()
+#f.exportToFile('asdasdasd')
