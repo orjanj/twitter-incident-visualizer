58d8e3d5d9bfe0f2fbf192b34303d2061aa006f2
Ørjan Jacobsen <orjan@orrie.org>
2020-04-02 03:51:51 +0200

    Finished lookUpLocation() convert from loc to latlong

--- classes/Gmaps.py
+++ classes/Gmaps.py
@@ -11,18 +11,20 @@ class Gmaps:
         """
         config = Config()
         config_param = config.getConfigParameter('gmaps')
-        self.gmaps_conf = gmaps.configure(api_key=config_param['api_key'])
 
-    def lookUpLocation(location_string):
+        self.gmaps = googlemaps.Client(key=config_param['api_key'])
+        self.country = config_param['country']
+
+    def lookUpLocation(self, location_string):
         """ Search for a location and return lat/long
         :param location_string: place names (string)
-        :return latlong: latitude and longitude (json str)
+        :return latlong: latitude and longitude (string)
         """
+        location_string = location_string + self.country
+        geocode_result = self.gmaps.geocode(location_string)
+        for data in geocode_result:
+            return(data['geometry']['location'])
 
-        # TODO: Use Geocoding Google API - URL: https://developers.google.com/maps/documentation/geocoding/usage-and-billing
-
-
-
-
-# REF:
-# https://github.com/googlemaps/google-maps-services-python
+# PoC
+#gm = Gmaps()
+#print(gm.lookUpLocation('Helligvaer')
