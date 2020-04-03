9378f9770a3b1fe6db7572d2d919215c964353bb
Ørjan Jacobsen <orjan@orrie.org>
2020-04-03 11:36:24 +0200

    Interface for pushing JSON structure

--- interface.py
+++ interface.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+import json
 from modules.Config import Config # TODO: Make it prettier
 from modules.DB import DB # TODO: Make it prettier
 from modules.Twitter import TIVTwitter # TODO: Make it prettier
@@ -12,6 +13,20 @@ if __name__ == '__main__':
     gmaps = Gmaps(config)
     twitter = TIVTwitter(config, db, gmaps)
 
-    # TODO: Dump to JSON FILE
-    #    {json lat long tweet text account name }
-    # export til json fil + les til jquery og legg til i google string
+    result = db.execute("select a.account_screen_name,a.account_pic_url, a.account_url, a.account_name, t.tweet_lat, t.tweet_long, t.tweet_time, t.tweet_content from account a, tweets t where a.account_id = t.account_id")
+    json_list = []
+    for row in result:
+        dct = {}
+        dct['screen_name'] = row[0].strip()
+        dct['profile_pic'] = row[1].strip()
+        dct['twitter_url'] = row[2].strip()
+        dct['account_name'] = row[3].strip()
+        dct['lat'] = row[4]
+        dct['lng'] = row[5]
+        dct['time'] = row[6].strip()
+        dct['tweet_content'] = row[7].strip()
+        json_list.append(dct)
+
+    json_dump = json.dumps(json_list, indent=2)
+    with open('examples/web_application.json', "w") as updated_file:
+        updated_file.write(json_dump)
