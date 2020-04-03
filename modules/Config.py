e66686dd2f2e6345c3cb5c9b829dc176baffb850
Ørjan Jacobsen <orjan@orrie.org>
2020-04-03 03:21:39 +0200

    Added comments to code

--- modules/Config.py
+++ modules/Config.py
@@ -8,6 +8,7 @@ class Config:
         """ Instance constructor
         :param filename: configuration file (yaml formated)
         """
+        # Set the filename for the config
         self.filename = filename
 
         # Append each config parameter to an list
@@ -17,11 +18,13 @@ class Config:
         """ Read file and add config parameters to instance
         :param filename: config file (yaml formated)
         """
+        # Open the configuration file and set config parameters
         with open(self.filename, 'r') as yml_file:
             self.config_json = yaml.load(yml_file)
 
-    def getConfigParameter(self, config_section): # todo: remove config_param
+    def getConfigParameter(self, config_section):
         """ Accessor method for displaying configuration parameters as requested.
         :return: parameter (str)
         """
+        # Return given config parameters as asked for
         return(self.config_json[config_section])
